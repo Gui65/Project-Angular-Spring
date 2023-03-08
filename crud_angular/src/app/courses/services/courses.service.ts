2f8506222c863a37d715201a78b57791458e088e
Guilherme Matos de Carvalho <guilhermematos851@gmail.com>
2023-03-08 16:17:08 -0300

    add Spinner

--- crud_angular/src/app/courses/services/courses.service.ts
+++ crud_angular/src/app/courses/services/courses.service.ts
@@ -1,8 +1,8 @@
-import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { delay, first, tap } from 'rxjs/operators';
 
 import { Course } from '../model/course';
-import { tap } from 'rxjs/operators'
 
 @Injectable({
   providedIn: 'root'
@@ -16,7 +16,9 @@ export class CoursesService {
   list() {
     return this.httpClient.get<Course[]>(this.API)
     .pipe(
+      first(),
+      delay(5000),
       tap(courses => console.log(courses))
-    )
+    );
   }
 }
