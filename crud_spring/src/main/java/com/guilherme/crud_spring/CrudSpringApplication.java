a93f58593f37687fcdb44977c8c067f10a440214
Guilherme Matos de Carvalho <guilhermematos851@gmail.com>
2023-03-09 15:15:34 -0300

    Listar Cursos API HTTP GET

--- crud_spring/src/main/java/com/guilherme/crud_spring/CrudSpringApplication.java
+++ crud_spring/src/main/java/com/guilherme/crud_spring/CrudSpringApplication.java
@@ -1,13 +1,31 @@
 package com.guilherme.crud_spring;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.guilherme.crud_spring.model.Course;
+import com.guilherme.crud_spring.repository.CourseRepository;
 
 @SpringBootApplication
 public class CrudSpringApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CrudSpringApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner initDataBase(CourseRepository courseRepository) {
+		return args -> {
+			courseRepository.deleteAll();
+
+			Course course = new Course();
+			course.setName("Angular com Spring");
+			course.setCategory("Front-End");
+
+			courseRepository.save(course);
+		};
 	}
 
 }
---
+++ crud_spring/src/main/java/com/guilherme/crud_spring/controller/CourseController.java
@@ -0,0 +1,28 @@
+package com.guilherme.crud_spring.controller;
+
+import java.util.List;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.guilherme.crud_spring.model.Course;
+import com.guilherme.crud_spring.repository.CourseRepository;
+
+import lombok.AllArgsConstructor;
+
+@RestController
+@RequestMapping("/api/courses")
+@AllArgsConstructor
+public class CourseController {
+
+    private final CourseRepository courseRepository;
+
+    @GetMapping
+    public List<Course> list() {
+        return courseRepository.findAll();
+    }
+
+}
