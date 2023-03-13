96f6b9ac54077dec90a26c52ba526e8fa22370e8
Guilherme Matos de Carvalho <guilhermematos851@gmail.com>
2023-03-13 10:47:49 -0300

    Conectando Angular na API Spring

--- crud_spring/src/main/java/com/guilherme/crud_spring/controller/CourseController.java
+++ crud_spring/src/main/java/com/guilherme/crud_spring/controller/CourseController.java
@@ -2,8 +2,6 @@ package com.guilherme.crud_spring.controller;
 
 import java.util.List;
 
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
