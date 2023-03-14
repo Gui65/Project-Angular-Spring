3e528635fed8dd3ab1b3644499e39ef6823fd44b
Guilherme Matos de Carvalho <guilhermematos851@gmail.com>
2023-03-14 14:29:24 -0300

    Criando metodo POST

--- crud_spring/src/main/java/com/guilherme/crud_spring/controller/CourseController.java
+++ crud_spring/src/main/java/com/guilherme/crud_spring/controller/CourseController.java
@@ -2,8 +2,13 @@ package com.guilherme.crud_spring.controller;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.guilherme.crud_spring.model.Course;
@@ -21,6 +26,16 @@ public class CourseController {
     @GetMapping
     public List<Course> list() {
         return courseRepository.findAll();
+    }
+
+    @PostMapping
+    @ResponseStatus(code = HttpStatus.CREATED)
+    public Course create(@RequestBody Course course) {
+        // System.out.println(course.getName());
+        // return courseRepository.save(course);
+        // return ResponseEntity.status(HttpStatus.CREATED)
+        // .body(courseRepository.save(course));
+        return courseRepository.save(course);
     }
 
 }
